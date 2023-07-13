602900d07ccd8299ddbf57e5b7a5aa83f1ee42ff
YeyoM <yeyomoreno2003@hotmail.com>
2023-07-13 11:36:46 -0600

    Fetching data (projects) directly from database and not hardcode into __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -58,43 +58,8 @@ def travel():
 
 @app.route('/projects')
 def projects():
-    projects = [
-        {
-            "id": "1",
-            "title": "Lofi Terminal",
-            "image": "../static/projects/Lofi_terminal.png",
-            "description": "This lofi player will make every developer feel like home, use it for those long coding sessions, built with NextJs and react-terminal. Check it out at: ",
-            "link": "https://lofi-terminal.vercel.app/"
-        },
-        {
-            "id": "2",
-            "title": "MazeSolver",
-            "image": "../static/projects/MazeSolver.png",
-            "description": "Maze Solver built with python. This project uses various techniques such as DFS, BFS, Dijkstra and A* Star to generate and solve a maze. Browse the code ",
-            "link": "https://github.com/YeyoM/mazeSolver"
-        },
-        {
-            "id": "3",
-            "title": "Past Portfolio",
-            "image": "../static/projects/Past_Portfolio.png",
-            "description": "This was my past portfolio I built with React, it features a brutalist design I personally love. Check it out at: ",
-            "link": "https://yeyom.github.io/yeyo_portfolio/"
-        },
-        {
-            "id": "4",
-            "title": "Ye_Quotes",
-            "image": "../static/projects/Ye_Quotes.png",
-            "description": "Ever thought of what Kanye has said on social media? No? Neither did I, but you can use it when you don't have anything to tweet, or maybe for your Instagram post's description, enjoy it! Check it out at: ",
-            "link": "https://yeyom.github.io/kanye_quotes/"
-        },
-        {
-            "id": "5",
-            "title": "Cocktail Please",
-            "image": "../static/projects/Cocktail_Please.png",
-            "description": "Do you love to try out new Cocktails? This is the perfect app for you. It will recommend you a random cocktail every week. Check it out at: ",
-            "link": "https://cocktail-please.vercel.app/"
-        }
-    ]
+    req = get_projects()
+    projects = req["projects"]
     return render_template('projects.html', projects = projects)
 
 @app.route('/experience')
