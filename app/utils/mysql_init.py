fafd41022c152379d2788ae4c397a275a1a647da
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-07-19 15:01:25 -0500

    ⚙️ Modified mysql_init to support unit testing db

--- app/utils/mysql_init.py
+++ app/utils/mysql_init.py
@@ -1,17 +1,49 @@
 import os 
 import datetime
 from peewee import *
+from dotenv import load_dotenv
 
-# MYSQL Connection
-mydb = MySQLDatabase(
-        os.getenv("MYSQL_DATABASE"),
-        user = os.getenv("MYSQL_USER"),
-        password = os.getenv("MYSQL_PASSWORD"),
-        host = os.getenv("MYSQL_HOST"),
-        port = 3306
-    )
+# Load environment variables --> Done for testing purposes
+load_dotenv()
 
-# Models
+# Initialize database as None
+mydb = None
+
+# Function to get database connection depending on the environment (Testing or not)
+def get_database():
+    global mydb
+
+    if mydb is None:
+        if os.getenv("TESTING") == "true":
+            print("Running in test mode")
+            mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+        else:
+            mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                user=os.getenv("MYSQL_USER"),
+                password=os.getenv("MYSQL_PASSWORD"),
+                host=os.getenv("MYSQL_HOST"),
+                port=3306
+            )
+    
+    return mydb
+
+# Function to initialize database to prevent when testing
+def db_int():
+    # Get database connection
+    db = get_database()
+    
+    if os.getenv("TESTING") == "true":
+            db.init('file:memory?mode=memory&cache=shared')
+    else:
+        db.init(os.getenv("MYSQL_DATABASE"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            host=os.getenv("MYSQL_HOST"),
+            port=3306
+        )
+    
+
+# Models used to create tables
 class TimeLinePost(Model):
     name = CharField()
     email = CharField()
@@ -19,7 +51,7 @@ class TimeLinePost(Model):
     created_at = DateTimeField(default = datetime.datetime.now)
 
     class Meta:
-        database = mydb
+        database = get_database()
 
 class Projects(Model):
     title = CharField()
@@ -29,20 +61,29 @@ class Projects(Model):
     created_at = DateTimeField(default = datetime.datetime.now)
 
     class Meta:
-        database = mydb
+        database = get_database()
+
 
 def connect():
+    # Get the correct database connection
+    db = get_database()
+    
     try:
-        mydb.connect()
+        # Initialize database
+        db_int()
+        # Connect to the database given the previous variable
+        db.connect()
         print("Connected to MySQL")
     except OperationalError as e:
         print("Error connecting to MySQL DB")
         print(e)
 
 def create_tables():
+    db = get_database()
+
     try:
-        mydb.create_tables([TimeLinePost])
-        mydb.create_tables([Projects])
+        db.create_tables([TimeLinePost])
+        db.create_tables([Projects])
         print("Created tables successfully")
     except OperationalError as e:
         print("Error creating tables")
