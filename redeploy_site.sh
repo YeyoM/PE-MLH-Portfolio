248ec2213bf8b8d3004e43e48aa0d1b2b39ef264
YeyoM <yeyomoreno2003@hotmail.com>
2023-07-25 21:04:35 +0000

    :sparkles: Modified redeploy script, moved from using services to docker!

--- redeploy_site.sh
+++ redeploy_site.sh
@@ -8,8 +8,6 @@ echo "Updating git repo"
 # Update git repo
 git fetch && git reset origin/main --hard 
 
-echo "Installing py req"
-# Enter venv and install requirements
-source python3-virtualenv/bin/activate && pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
 
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
